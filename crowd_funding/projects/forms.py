51a5ef71944df2fa532b8387f12b17e006fcd990
Mustafa Badr <mustafabadr879@gmail.com>
2023-10-20 21:13:15 +0300

    add rating view

--- crowd_funding/projects/forms.py
+++ crowd_funding/projects/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Rating
+from projects.models import Rating
 
 class RatingForm(forms.ModelForm):
     class Meta:
--- crowd_funding/projects/views.py
+++ crowd_funding/projects/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Project, Rating
+from .forms import RatingForm
 
-# Create your views here.
+
+
 def create_project(request):
     pass
 
 
 
+@login_required
+def add_rating_view(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    
+    if request.method == 'POST':
+        form = RatingForm(request.POST)
+        if form.is_valid():
+
+            rating = form.save(commit=False)
+            rating.user = request.user
+            rating.project = project
+            rating.save()
+            project.add_rate(rating.rate)
+
+            return redirect('projects.show', project_id=project_id)
+    else:
+        form = RatingForm()
+
+    return render(request, 'add_rating.html', {'form': form, 'project': project})
 
 
 
