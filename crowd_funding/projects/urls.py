72febc62c1232a1d2917a4a1b4614fdae5d86e70
Mustafa Badr <mustafabadr879@gmail.com>
2023-10-20 21:14:08 +0300

    add rating url

--- crowd_funding/projects/urls.py
+++ crowd_funding/projects/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
-from projects.views import create_project
+from projects.views import create_project,add_rating_view
+
 urlpatterns=[
     path('create/',create_project,name='project.create'),
-
+    path('projects/<int:project_id>/rate/', add_rating_view, name='add_rating'),
 
 
 ]
+
