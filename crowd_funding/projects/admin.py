97e96bdc4635013032cb038afcd6f7566db190c3
Mustafa Badr <mustafabadr879@gmail.com>
2023-10-20 21:35:31 +0300

    add rate to the admin panel

--- crowd_funding/projects/admin.py
+++ crowd_funding/projects/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from projects.models import Project,Category
+from projects.models import Project,Category,Rating
 
 admin.site.register(Project)
 admin.site.register(Category)
+admin.site.register(Rating)
