985a326b340fe1e127a9c29eeb8a6d89dafef401
AhmeedKadry <ahmed.Kadry.ramadan@gmail.com>
2023-10-21 18:52:48 +0300

    create method to send email activation for user

--- crowd_funding/accounts/views.py
+++ crowd_funding/accounts/views.py
@@ -1,7 +1,35 @@
 from django.views.generic.edit import  CreateView
 from django.urls import reverse_lazy
+from django.contrib import messages
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.core.mail import EmailMessage
+from .tokens import account_activation_token
 from .forms import AccountForm
 from .models import UserProfile
+
+
+
+def activateEmail(request, user, to_email):
+    mail_subject = "Activate your user account."
+    
+    message = render_to_string("registration/template_activate_account.html", {
+        'user': user.username,
+        'domain': get_current_site(request).domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': account_activation_token.make_token(user),
+        "protocol": 'https' if request.is_secure() else 'http'
+    })
+    email = EmailMessage(mail_subject, message, to=[to_email])
+    if email.send():
+        messages.success(request, f'Dear <b>{user}</b>, please go to you email <b>{to_email}</b> inbox and click on \
+                received activation link to confirm and complete the registration. <b>Note:</b> Check your spam folder.')
+    else:
+        messages.error(request, f'Problem sending email to {to_email}, check if you typed it correctly.')
+
+
 
 # create new user ?
 class AccountCreateView(CreateView):
