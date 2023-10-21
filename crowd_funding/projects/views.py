891ea52e1a2522b0e45ec01ad41b24e1b6950eae
Mustafa Badr <mustafabadr879@gmail.com>
2023-10-21 23:03:23 +0300

    add changes to project details html.

--- crowd_funding/projects/views.py
+++ crowd_funding/projects/views.py
@@ -47,6 +47,5 @@ def searchProject(request):
 def ViewProject(request,id):  
     filteredProject = Project.objects.get(id=id)
 
-    
     return render(request, 'proj/projectDetails.html', context={"project": filteredProject,})
  
