acbc16917462445163acc56b0b9252ee047a5997
AhmeedKadry <ahmed.Kadry.ramadan@gmail.com>
2023-10-21 19:22:51 +0300

    create URLs for registration success, activation success, activation failure and activate account

--- crowd_funding/accounts/urls.py
+++ crowd_funding/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import include
 from django.urls import path
-from accounts.views import AccountCreateView, activate
+from accounts.views import AccountCreateView, ActivationSuccessView, ActivationFailureView, ActivationRequiredView, activate
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('register', AccountCreateView.as_view(), name='accounts.register'),
+    path('register/success', ActivationRequiredView.as_view(), name='activation.required'),
     path('activate/<uidb64>/<token>', activate, name='activate'),
-    
+    path('activation/success/', ActivationSuccessView.as_view(), name='registration_success'),
+    path('activation/failure/', ActivationFailureView.as_view(), name='registration_failure'),
+
 ]
