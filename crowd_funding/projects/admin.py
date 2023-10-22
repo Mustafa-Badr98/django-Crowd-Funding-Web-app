79c464c015092d320fd2c0a0930252eb73f825b9
Mustafa Badr <mustafabadr879@gmail.com>
2023-10-22 08:37:17 +0300

    add reportProject and reportComment models to the admin panel also.

--- crowd_funding/projects/admin.py
+++ crowd_funding/projects/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from projects.models import Project,Category,Rating,Comment
+from projects.models import Project,Category,Rating,Comment,ReportedProject,ReportedComment
 
 admin.site.register(Project)
 admin.site.register(Category)
 admin.site.register(Rating)
 admin.site.register(Comment)
+admin.site.register(ReportedComment)
+admin.site.register(ReportedProject)
