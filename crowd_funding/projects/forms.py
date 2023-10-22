f4de0c04cb809648cb51322aebac7cb31cf2d6bf
Mustafa Badr <mustafabadr879@gmail.com>
2023-10-22 08:58:10 +0300

    add funding form

--- crowd_funding/projects/forms.py
+++ crowd_funding/projects/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from projects.models import Rating
+from projects.models import Rating,Funding
 
 class RatingForm(forms.ModelForm):
     class Meta:
         model = Rating
         fields = ['rate_value']
+
+
+class FundingForm(forms.ModelForm):
+    class Meta:
+        model = Funding
+        fields = ['amount']
