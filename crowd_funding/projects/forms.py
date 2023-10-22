430bc36de33571bee9095c205ef6515f54f818d5
msabour11 <mohamedabdo2611@gmail.com>
2023-10-22 17:15:11 +0300

    remove fields from form

--- crowd_funding/projects/forms.py
+++ crowd_funding/projects/forms.py
@@ -11,4 +11,4 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
 
-        fields=['title', 'details', 'Category','total_target','start_date','end_date','current_fund','num_of_ratings','total_rate','average_rate','tag1','tag2','tag3','tag4','image1','image2','image3','image4','main_image']
+        fields=['title', 'details', 'Category','total_target','start_date','end_date','current_fund','image1','image2','image3','image4','main_image']
--- crowd_funding/projects/urls.py
+++ crowd_funding/projects/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
-from projects.views import create_project,add_rating_view,searchProject
+from projects.views import create_project,add_rating_view,searchProject,edit_project
 
 urlpatterns=[
     path('create/',create_project,name='project.create'),
+    path('edit/<int:id>', edit_project, name='projects.edit'),
+
     path('<int:project_id>/rate/', add_rating_view, name='add_rating'),
     path('searched/',searchProject  ,name="project.search"),
 
