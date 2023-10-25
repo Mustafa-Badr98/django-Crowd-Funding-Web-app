bef3e9537797c68fd0fa21b834f383c4e5ebf0b2
msabour11 <mohamedabdo2611@gmail.com>
2023-10-25 23:00:15 +0300

    remove current fund and end date from form

--- crowd_funding/projects/forms.py
+++ crowd_funding/projects/forms.py
@@ -11,7 +11,7 @@ class RatingForm(forms.ModelForm):
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        fields=['title', 'details', 'Category','total_target','start_date','end_date','current_fund','image1','image2','image3','image4','main_image']
+        fields=['title', 'details', 'Category','total_target','start_date','image1','image2','image3','image4','main_image']
 
 class ReportCommentForm(forms.ModelForm):
     class Meta:
