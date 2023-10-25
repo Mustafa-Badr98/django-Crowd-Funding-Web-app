e63ee5060d777784b2976944c4e0448ba9f512bc
msabour11 <mohamedabdo2611@gmail.com>
2023-10-25 13:00:31 +0300

    add report project url

--- crowd_funding/projects/urls.py
+++ crowd_funding/projects/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from projects.views import searchProject,edit_project
-from projects.views import create_project,add_rating_view2,searchProject,ViewProject,add_comment,fund_project,CategoryProjectsListView,cancel_project
+from projects.views import create_project,add_rating_view2,searchProject,ViewProject,add_comment,fund_project,CategoryProjectsListView,cancel_project,report_project
 
 urlpatterns=[
     path('create/',create_project,name='project.create'),
@@ -12,6 +12,7 @@ urlpatterns=[
     path('fund/<int:project_id>', fund_project,name='project_add_fund'),
     path('viewCategory/<int:category_id>', CategoryProjectsListView.as_view(),name='category.show'),
     path('cancel/<int:id>', cancel_project, name='projects.cancel'),
+    path('report/<int:id>', cancel_project, name='projects.report'),
 
 ]
 
