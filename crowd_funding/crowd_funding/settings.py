fd75b3c1a1730ef6b10000d321d77e9b1f298f81
AhmeedKadry <ahmed.Kadry.ramadan@gmail.com>
2023-10-30 22:47:30 +0200

    fix Bug

--- crowd_funding/crowd_funding/settings.py
+++ crowd_funding/crowd_funding/settings.py
@@ -92,18 +92,11 @@ WSGI_APPLICATION = 'crowd_funding.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test_dj_pro',
-        'USER': 'dj_pro_iti',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    
 }
 
 
